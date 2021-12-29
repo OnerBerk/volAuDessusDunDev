8a5f729aa0cfd12a4514e98892e45b2052ad5caf
OnerBerk <oner.berk@joza-it.fr>
2021-12-29 23:24:09 +0100

    init node expres typescript linter prettier

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,2 +1,13 @@
-const msg="hello worldi"
-console.log(msg)
+import express from 'express';
+
+const app = express();
+const PORT = 8080 || process.env;
+const global_routes = require('./routes/global-routes');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(` api listen on ${PORT}`);
+});
+app.use('/api/v1', global_routes);
